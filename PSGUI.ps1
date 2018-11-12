fa616efe39158ee476b9c3011b8971b3604fc219
Mentaleak <mentaleak@gmail.com>
2018-11-12 14:02:21 -0500

    Modified PSGUI.ps1
    Changed functions:
    Get-PSObjectParamTypes

--- PSGUI.ps1
+++ PSGUI.ps1
@@ -1,37 +1,37 @@
-﻿$TestObject= [PSCustomObject]@{
-String="test"
-Int = 23
-Double=23.1
-Bool=$true
+﻿$TestObject = [pscustomobject]@{
+	String = "test"
+	Int = 23
+	Double = 23.1
+	Bool = $true
 }
 
 
 
-$TestObjectB= [PSCustomObject]@{
-String=[string]
-Int = [int]
-Double=[double]
-Bool=[bool]
-datetime=[datetime]
+$TestObjectB = [pscustomobject]@{
+	String = [string]
+	Int = [int]
+	Double = [double]
+	Bool = [bool]
+	datetime = [datetime]
 }
 #This is a test
 
-function Get-PSObjectParamTypes(){
-param(
-    $Object
-)
-    $NoteProperties =$object | get-member -MemberType NoteProperty
-   foreach($property in $NoteProperties)
-   {
-   $Pdefinition = $property.Definition.split(" ")
-   $PType = $Pdefinition[0]
-       if($PType -eq "RuntimeType"){
-            $PType=$Pdefinition[1].split("=")[0]
-        }
+function Get-PSObjectParamTypes () {
+	param(
+		$Object
+	)
+	$NoteProperties = $object | Get-Member -MemberType NoteProperty
+	foreach ($property in $NoteProperties)
+	{
+		$Pdefinition = $property.Definition.split(" ")
+		$PType = $Pdefinition[0]
+		if ($PType -eq "RuntimeType") {
+			$PType = $Pdefinition[1].split("=")[0]
+		}
 
-   Add-Member -InputObject $property -MemberType NoteProperty -name "Type" -Value $PType
-   }
+		Add-Member -InputObject $property -MemberType NoteProperty -Name "Type" -Value $PType
+	}
 
-    return $NoteProperties
+	return $NoteProperties
 }
 
