97121435812e852516d2bbf638e0214576d52781
Mentaleak <mentaleak@gmail.com>
2018-11-12 12:28:10 -0500

    Added PSGUI.ps1
    Changed functions:
    Get-PSObjectParamTypes

---
+++ PSGUI.ps1
@@ -0,0 +1,35 @@
+﻿$TestObject= [PSCustomObject]@{
+String="test"
+Int = 23
+Double=23.1
+Bool=$true
+}
+
+$TestObjectB= [PSCustomObject]@{
+String=[string]
+Int = [int]
+Double=[double]
+Bool=[bool]
+datetime=[datetime]
+}
+
+
+function Get-PSObjectParamTypes(){
+param(
+    $Object
+)
+    $NoteProperties =$object | get-member -MemberType NoteProperty
+   foreach($property in $NoteProperties)
+   {
+   $Pdefinition = $property.Definition.split(" ")
+   $PType = $Pdefinition[0]
+       if($PType -eq "RuntimeType"){
+            $PType=$Pdefinition[1].split("=")[0]
+        }
+
+   Add-Member -InputObject $property -MemberType NoteProperty -name "Type" -Value $PType
+   }
+
+    return $NoteProperties
+}
+
