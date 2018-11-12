44cd928f2a831ced50c7ef0e44cf802f049fd12c
Mentaleak <mentaleak@gmail.com>
2018-11-12 15:29:59 -0500

    Modified PSGUI.ps1
    Changed functions:
    Get-PSObjectParamTypes
    Show-Psgui

--- PSGUI.ps1
+++ PSGUI.ps1
@@ -35,3 +35,45 @@ function Get-PSObjectParamTypes () {
 	return $NoteProperties
 }
 
+function Show-Psgui () {
+	param(
+		$object,
+		[int]$height = 600,
+		[int]$width = 600
+	)
+	$Form = New-Object system.Windows.Forms.Form
+	#TODO automate width and height
+	$Form.ClientSize = '$width,$height'
+	$Form.text = "Form"
+	$Form.TopMost = $false
+
+	$NoteProperties = Get-PSObjectParamTypes $object
+	$currentX = 15
+	$currentY = 15
+	$fields = @()
+	$fieldCount = 0
+	foreach ($NP in $NoteProperties)
+	{
+		switch ($NP.Type)
+		{
+			bool
+			{
+				New-Variable -Name "Checkbox_$($np.Name)" -Value (New-Object system.Windows.Forms.CheckBox)
+				(Get-Variable -Name "Checkbox_$($np.Name)")
+				(Get-Variable -Name "Checkbox_$($np.Name)").text = "checkBox"
+				(Get-Variable -Name "Checkbox_$($np.Name)").AutoSize = $false
+				(Get-Variable -Name "Checkbox_$($np.Name)").width = 95
+				(Get-Variable -Name "Checkbox_$($np.Name)").height = 20
+				(Get-Variable -Name "Checkbox_$($np.Name)").location = New-Object System.Drawing.Point ($currentX,$currentY)
+				(Get-Variable -Name "Checkbox_$($np.Name)").Font = 'Microsoft Sans Serif,10'
+			}
+
+
+
+		}
+		$currentX += 25
+
+	}
+
+
+}
